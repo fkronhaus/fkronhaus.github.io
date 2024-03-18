5ecca6ff8599259cbcd7be6574b33b8433518591
Federico Kronhaus <federicokronhaus@gmail.com>
2024-03-18 11:59:23 -0300

    Autenticación con algunas traducciones

---
+++ beneficios/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Su contraseña ha sido reestablecida.',
+    'sent' => 'Le hemos enviado un link para restablecer su contraseña.',
+    'throttled' => 'Por favor espere antes de intentarlo nuevamente.',
+    'token' => 'El token para restablecer su password es inválido..',
+    'user' => "No pudimos encontrar un usuario con ese correo electronico.",
+
+];
